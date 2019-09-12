f897ed2a9358447ad7376979b46acae6086836bf
Sai Allala <skrallala752@gmail.com>
2019-09-12 13:34:07 -0400

    added a few css files amd java script files for bootstrap

--- js/homepage_form.js
+++ js/homepage_form.js
@@ -1,51 +1,58 @@
 'use strict';
 // import {saveImage}  from "./modules/local_storage.js"; // test me
 var changes = {};
+
 function save_school_page() {
-  saveToCookies(changes);
+    saveToCookies(changes);
 }
+
 function change_primary_color_html(choice) {
-  var color_formatted = choice.value.replace(" ", "");
-  if (isColor(color_formatted)) {
-    document.body.style.backgroundColor = color_formatted;
-    changes['primary_color'] = color_formatted;
-  }
+    var color_formatted = choice.value.replace(" ", "");
+    if (isColor(color_formatted)) {
+        document.body.style.backgroundColor = color_formatted;
+        changes['primary_color'] = color_formatted;
+    }
 }
+
 function change_primary_color(choice) {
-  var hex_color = "#" + choice;
-  document.body.style.backgroundColor = hex_color;
-  changes['primary_color'] = hex_color;
+    var hex_color = "#" + choice;
+    document.body.style.backgroundColor = hex_color;
+    changes['primary_color'] = hex_color;
 }
+
 function change_secondary_color_html(choice) {
-  var color_formatted = choice.value.replace(" ", "");
-  if (isColor(color_formatted)) {
-    document.body.style.color = color_formatted;
-    changes['secondary_color'] = color_formatted;
-  }
+    var color_formatted = choice.value.replace(" ", "");
+    if (isColor(color_formatted)) {
+        document.body.style.color = color_formatted;
+        changes['secondary_color'] = color_formatted;
+    }
 }
+
 function change_secondary_color(choice) {
-  var hex_color = "#" + choice;
-  document.body.style.color = hex_color;
-  changes['secondary_color'] = hex_color;
+    var hex_color = "#" + choice;
+    document.body.style.color = hex_color;
+    changes['secondary_color'] = hex_color;
 }
+
 function changeTitle(choice) {
-  document.getElementsByTagName('h1')[0].innerText = choice.value;
-  changes['title'] = choice.value;
+    document.getElementsByTagName('h1')[0].innerText = choice.value;
+    changes['title'] = choice.value;
 }
+
 function getLogo(choice) { // recieve file
-  var logo = document.getElementById('logo'); // get logo spot
-  var reader = new FileReader();
-  reader.onload = function (e) {
-  // get loaded data and render thumbnail.
-    logo.src = e.target.result; // data uri
-    logo.alt = choice.files[0].name;
-  };
-  // read the image file as a data URL.
-  reader.readAsDataURL(choice.files[0]);
-  saveImage(logo, 'logo');
+    var logo = document.getElementById('logo'); // get logo spot
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        // get loaded data and render thumbnail.
+        logo.src = e.target.result; // data uri
+        logo.alt = choice.files[0].name;
+    };
+    // read the image file as a data URL.
+    reader.readAsDataURL(choice.files[0]);
+    saveImage(logo, 'logo');
 }
 const isColor = (strColor) => { // change to function notation?
-  const s = new Option().style;
-  s.color = strColor;
-  return s.color !== '';
+    const s = new Option().style;
+    s.color = strColor;
+    return s.color !== '';
 }
